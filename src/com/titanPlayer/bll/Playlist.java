8779274b8d9e8d43e066c5675eff78f0af52b725
Kyle <Kyle@192.168.1.105>
2012-11-27 19:19:41 -0500

    Added user requirement 13: sort by title/author

--- src/com/titanPlayer/bll/Playlist.java
+++ src/com/titanPlayer/bll/Playlist.java
@@ -11,26 +11,12 @@ package com.titanPlayer.bll;
 public class Playlist {
     
     private String playlistName;
-    private int currentSongIndex;
     
     public Playlist(String nameOfList) {
         this.playlistName = nameOfList;
-        this.currentSongIndex = 0;
     }
     
     public String getName() {
         return this.playlistName;
-    }
-    
-    public void skipSong() {
-        this.currentSongIndex++;
-    }
-    
-    public int getCurrentSongIndex() {
-        return this.currentSongIndex;
-    }
-    
-    public void stopSong() {
-        this.currentSongIndex = -1;
     }
 }
